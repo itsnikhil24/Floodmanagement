f0dab5fb996b444b408e478fa9273ba800e09615
itsnikhil24 <nikhils.sharma2409@gmail.com>
2026-06-24 15:05:22 +0530

    Vercel deployment fix kyuki render ne apni aukaat dikha di

--- index.mjs
+++ index.mjs
@@ -29,15 +29,15 @@ let bucket;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(process.cwd(), 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware setup
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection setup with GridFSBucket
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
